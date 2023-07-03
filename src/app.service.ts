da411085eedfb9d7f6819cd5166cdc142b0616a3
Cesar Flores <sesar.fls@gmail.com>
2023-07-03 01:08:04 -0600

    doker

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'On Production';
+  }
+
+  getHello2(): string {
+    return 'On Production Dos';
   }
 }
