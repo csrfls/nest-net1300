ced5aaa3dd1ef77b62449bbcc1e1cf760edf449e
Cesar Flores <sesar.fls@gmail.com>
2023-07-03 02:43:29 -0600

    Actualizar app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Deploy Push On Production';
+    return 'Deploy Push On Production latest';
   }
 
   getHello2(): string {
